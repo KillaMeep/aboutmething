c829dd15ac2871beaa958aec6769f9884836d0f7
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2021-11-29 18:00:32 -0700

    Add files via upload

---
+++ updater.bat
@@ -0,0 +1,11 @@
+@echo off
+del killos.py
+del launcher.bat
+del requirements.txt
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/killos.py -O
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/requirements.txt -O
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/launcher.bat -O
+pip install -r requirements.txt
+del requirements.txt
+:eof
+echo done
