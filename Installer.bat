1627ee5d02bf6138cdc36f32ea53fcd44a5f4ef9
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2021-11-29 18:05:54 -0700

    Update Installer.bat

--- Installer.bat
+++ Installer.bat
@@ -1,12 +1,8 @@
 @echo off
-IF EXIST killos.py GOTO :eof
-ELSE
 curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/killos.py -O
 curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/requirements.txt -O
 curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/launcher.bat -O
-curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/updater.bat
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/updater.bat -O
 pip install -r requirements.txt
 del requirements.txt
 del Installer.bat
-:eof
-echo done
