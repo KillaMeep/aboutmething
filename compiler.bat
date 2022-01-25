907569c4d96abdc89498b89cf7a432f645a8483b
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-01-25 01:31:09 -0700

    Add files via upload

--- compiler.bat
+++ compiler.bat
@@ -1,31 +1,15 @@
-@echo off
-title Compiler
-mkdir compiler
-cd compiler
-curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/compiler.zip -O && tar -xf compiler.zip && del compiler.zip
-curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/Installer.bat -O && start /W /b Installer.bat
-cd killos
-echo %CD% >> path.txt
-move path.txt ..
-cd ..
-move path.txt compiler
-cd compiler
+@ECHO OFF
+del killos.exe
+mkdir build
+cd build
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/killos.py -O
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/requirements.txt -O
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/launcher.bat -O
+curl https://raw.githubusercontent.com/KillaMeep/aboutmething/main/updater.bat -O
 pip install -r requirements.txt
-start /b runcompiler.bat
-timeout 15
-clear
-cd build && cd killos
-move killos.exe ..
-cd ..
-move killos.exe ..
-cd ..
-move killos.exe ..
-cd ..
-move killos.exe ..
-cd ..
-echo @echo off >> delet.bat
-echo taskkill /f /fi "WINDOWTITLE eq builder" >> delet.bat
-echo RD /S /Q "compiler" >> delet.bat
-echo del compiler.bat >> delet.bat
-echo del delet.bat >> delet.bat
-start delet.bat
+pip install auto-py-to-exe
+del requirements.txt
+pyinstaller --noconfirm --onefile --console killos.py
+cd dist && copy killos.exe ..\.. && cd ..\..
+rmdir /Q /S build
+exit
