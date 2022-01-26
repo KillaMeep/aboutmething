2b693513045468ee3ed9ccb7d0eaff875d9e923a
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-01-26 03:46:50 -0700

    Update pythoninstaller.bat

--- pythoninstaller.bat
+++ pythoninstaller.bat
@@ -1,8 +1,10 @@
-echo installing python now.
+echo Running python installer now.
+TITLE Python Installer
 IF EXIST pyinstaller.exe DEL /F pyinstaller.exe
-start /B /wait curl https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -o pyinstaller.exe && goto finishinstall
-echo finishing python install
-start /B /wait pyinstaller.exe /quiet InstallAllUsers=1 PrependPath=1
+curl https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -o pyinstaller.exe
+echo running installer now.
+pyinstaller.exe /quiet InstallAllUsers=1 PrependPath=1
 del pyinstaller.exe
 pip install wheel
 echo python install done.
+exit
