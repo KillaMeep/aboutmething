6fc8c0b804e423c05d4f3d6242e004fccab8b6d2
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-01-26 05:39:51 -0700

    Update pythoninstaller.bat

--- pythoninstaller.bat
+++ pythoninstaller.bat
@@ -35,7 +35,6 @@ echo Running python installer now.
 TITLE Python Installer
 IF EXIST pyinstaller.exe DEL /F pyinstaller.exe
 curl https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe -o pyinstaller.exe
-echo running installer now.
 pyinstaller.exe /quiet InstallAllUsers=1 PrependPath=1
 del pyinstaller.exe
 pip install wheel
