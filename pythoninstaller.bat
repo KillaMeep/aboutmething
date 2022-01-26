1029446b260252701089caf66a8e77a7710dd753
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-01-26 05:38:26 -0700

    Update pythoninstaller.bat

--- pythoninstaller.bat
+++ pythoninstaller.bat
@@ -31,8 +31,6 @@ cd /d %~dp0
 if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
 
 ::start
-taskkill /f /fi "WINDOWTITLE eq Administrator: KillOS Compiler*"
-taskkill /f /fi "WINDOWTITLE eq KillOS Compiler*"
 echo Running python installer now.
 TITLE Python Installer
 IF EXIST pyinstaller.exe DEL /F pyinstaller.exe
